570421577b77ad03f05b2fe28da23953d2831af8
mpho061 <monyamanempho13@gmail.com>
2022-05-18 15:19:26 +0200

    added my project

--- js/slidejs.js
+++ js/slidejs.js
@@ -1,23 +1,15 @@
-const labels = document.querySelectorAll('.form-control label')
 
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-    .join('')
-})
+var myIndex = 0;
+carousel();
 
-let slideIndex=0;
-showslides();
-function showslides(){
-    let i;
-    let slides=
-    document.getElementsByClassName("mySlides");
-    for (i=0; i <slides.length; i++) {
-        slides[i].style.display="none"
-    }
-    slideIndex++;
-    if (slideIndex>slides.length){slideIndex=1}
-    slides[slideIndex-1].style.display="block";
- setTimeout(showslides,3000);
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
 }
